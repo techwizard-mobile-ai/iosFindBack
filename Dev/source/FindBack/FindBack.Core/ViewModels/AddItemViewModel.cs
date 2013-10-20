3d10d7191745bf08e87f79af7fd9dd7c18f9e343
the-raccoondog <dylan1406lewis@gmail.com>
2013-10-21 00:35:48 +0200

    Adding Picture Taking

--- Dev/source/FindBack/FindBack.Core/ViewModels/AddItemViewModel.cs
+++ Dev/source/FindBack/FindBack.Core/ViewModels/AddItemViewModel.cs
@@ -1,30 +1,74 @@
 ﻿namespace FindBack.Core.ViewModels
 {
-    using System.Collections.ObjectModel;
+    using System.IO;
 
-    using Cirrious.CrossCore;
-    using Cirrious.MvvmCross.Plugins.Location;
     using Cirrious.MvvmCross.Plugins.Messenger;
+    using Cirrious.MvvmCross.Plugins.PictureChooser;
     using Cirrious.MvvmCross.ViewModels;
 
-    using FindBack.Core.Model;
-    using FindBack.Core.Services;
     using FindBack.Core.Services.Location;
 
     public class AddItemViewModel : MvxViewModel
     {
         private ILocationService _locationService;
-
+        private readonly IMvxPictureChooserTask _pictureChooserTask;
         private readonly IMvxMessenger _messenger;
-
         private readonly MvxSubscriptionToken _token;
 
         private double _longitude;
         private double _latitude;
 
-        public AddItemViewModel(ILocationService locationService, IMvxMessenger messenger)
+        private byte[] _bytes;
+
+        private MvxCommand _choosePictureCommand;
+
+        private MvxCommand _takePictureCommand;
+
+        public AddItemViewModel(ILocationService locationService, IMvxMessenger messenger, IMvxPictureChooserTask pictureChooserTask)
         {
             _token = messenger.Subscribe<LocationMessage>(OnLocationMessage);
+            _pictureChooserTask = pictureChooserTask;
+        }
+
+        public byte[] Bytes
+        {
+            get { return this._bytes; }
+            set { this._bytes = value; RaisePropertyChanged(() => Bytes); }
+        }
+
+        public System.Windows.Input.ICommand TakePictureCommand
+        {
+            get
+            {
+                _takePictureCommand = _takePictureCommand ?? new MvxCommand(DoTakePicture);
+                return _takePictureCommand;
+            }
+        }
+
+        public System.Windows.Input.ICommand ChoosePictureCommand
+        {
+            get
+            {
+                _choosePictureCommand = _choosePictureCommand ?? new MvxCommand(DoChoosePicture);
+                return _choosePictureCommand;
+            }
+        }
+
+        private void DoTakePicture()
+        {
+            this._pictureChooserTask.TakePicture(400, 95, this.OnPicture, () => { });
+        }
+
+        private void DoChoosePicture()
+        {
+            _pictureChooserTask.ChoosePictureFromLibrary(400, 95, OnPicture, () => { });
+        }
+
+        private void OnPicture(Stream pictureStream)
+        {
+            var memoryStream = new MemoryStream();
+            pictureStream.CopyTo(memoryStream);
+            Bytes = memoryStream.ToArray();
         }
 
         private void OnLocationMessage(LocationMessage locationMessage)
