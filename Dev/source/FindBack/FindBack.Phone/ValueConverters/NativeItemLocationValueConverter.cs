f8c49ece3a3e9df9ee353437acb6065f3c0e1708
the-raccoondog <dylan1406lewis@gmail.com>
2013-11-08 11:20:21 +0100

    - Removed unnecessary converters.
    - Fixing some layouts.

--- Dev/source/FindBack/FindBack.Phone/ValueConverters/NativeItemLocationValueConverter.cs
+++
@@ -1,9 +0,0 @@
-﻿using Cirrious.CrossCore.WindowsPhone.Converters;
-using FindBack.Core.Converters;
-
-namespace FindBack.Phone.ValueConverters
-{
-    public class NativeItemLocationValueConverter : MvxNativeValueConverter<ItemLocationConverter>
-    {
-    }
-}
